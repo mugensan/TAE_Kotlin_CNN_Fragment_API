8bd7d81f4af34a75f0ff1a729b962ec2a8df8a9a
Krom001 <user@example.com>
2019-08-30 15:41:12 +0100

    first commit

---
+++ app/src/main/java/com/example/tae_kotlin_cnn_fragment_api/model/Articles.kt
@@ -0,0 +1,17 @@
+package com.example.tae_kotlin_cnn_fragment_api.model
+
+import Source
+import com.google.gson.annotations.SerializedName
+
+
+data class Articles (
+
+	@SerializedName("source") val source : Source,
+	@SerializedName("author") val author : String,
+	@SerializedName("title") val title : String,
+	@SerializedName("description") val description : String,
+	@SerializedName("url") val url : String,
+	@SerializedName("urlToImage") val urlToImage : String,
+	@SerializedName("publishedAt") val publishedAt : String,
+	@SerializedName("content") val content : String
+)
---
+++ app/src/main/java/com/example/tae_kotlin_cnn_fragment_api/model/BaseModel.kt
@@ -0,0 +1,9 @@
+import com.example.tae_kotlin_cnn_fragment_api.model.Articles
+import com.google.gson.annotations.SerializedName
+
+data class BaseModel (
+
+	@SerializedName("status") val status : String,
+	@SerializedName("totalResults") val totalResults : Int,
+	@SerializedName("articles") val articles : List<Articles>
+)
