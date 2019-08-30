627559c9e69d2acae3347a5e1783fa4c892025cd
Krom001 <user@example.com>
2019-08-30 17:28:09 +0100

    login working

--- app/src/main/java/com/example/tae_kotlin_cnn_fragment_api/model/cnnmodel/CnnModel.kt
+++ app/src/main/java/com/example/tae_kotlin_cnn_fragment_api/model/cnnmodel/CnnModel.kt
@@ -1,7 +1,7 @@
 import com.example.tae_kotlin_cnn_fragment_api.model.Articles
 import com.google.gson.annotations.SerializedName
 
-data class BaseModel (
+data class CnnModel (
 
 	@SerializedName("status") val status : String,
 	@SerializedName("totalResults") val totalResults : Int,
---
+++ app/src/main/java/com/example/tae_kotlin_cnn_fragment_api/view/homepage/HomePageActivity.kt
@@ -0,0 +1,4 @@
+package com.example.tae_kotlin_cnn_fragment_api.view.homepage
+
+class HomePageActivity {
+}
